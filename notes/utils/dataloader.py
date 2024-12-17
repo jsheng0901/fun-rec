d05826ee30bcc273f28d2caf84d83c37d89c52a0
junzhi sheng <jsheng0901@gmail.com>
2024-12-16 22:43:42 -0500

    add new dataset loader

--- notes/utils/dataloader.py
+++ notes/utils/dataloader.py
@@ -79,9 +79,9 @@ def create_dataset(dataset='criteo', read_part=True, sample_num=100000, task='cl
     # elif dataset == 'movielens':
     #     return MovieLensDataset('../dataset/ml-latest-small-ratings.txt', read_part=read_part, sample_num=sample_num,
     #                             task=task).to(device)
-    # elif dataset == 'amazon-books':
-    #     return AmazonBooksDataset('../dataset/amazon-books-100k.txt', read_part=read_part, sample_num=sample_num,
-    #                               sequence_length=sequence_length).to(device)
+    elif dataset == 'amazon-books':
+        return AmazonBooksDataset('../../data/amazon-books-100k.txt', read_part=read_part, sample_num=sample_num,
+                                  sequence_length=sequence_length).to(device)
     else:
         raise Exception('No such dataset!')
 
@@ -159,6 +159,62 @@ class CriteoDataset(Dataset):
         data_df[features] = OrdinalEncoder().fit_transform(data_df[features])
 
         self.data = data_df[features + ['label']].values
+
+
+class AmazonBooksDataset(Dataset):
+
+    def __init__(self, file, read_part=True, sample_num=100000, sequence_length=40):
+        super().__init__()
+
+        if read_part:
+            data_df = pd.read_csv(file, sep=',', nrows=sample_num)
+        else:
+            data_df = pd.read_csv(file, sep=',')
+
+        data_df['hist_item_list'] = data_df.apply(lambda x: x['hist_item_list'].split('|'), axis=1)
+        data_df['hist_cate_list'] = data_df.apply(lambda x: x['hist_cate_list'].split('|'), axis=1)
+
+        # cate encoder
+        cate_list = list(data_df['cateID'])
+        data_df.apply(lambda x: cate_list.extend(x['hist_cate_list']), axis=1)
+        cate_set = set(cate_list + ['0'])
+        cate_encoder = LabelEncoder().fit(list(cate_set))
+        self.cate_set = cate_encoder.transform(list(cate_set))
+
+        # cate pad and transform
+        hist_limit = sequence_length
+        col = ['hist_cate_{}'.format(i) for i in range(hist_limit)]
+
+        def deal(x):
+            if len(x) > hist_limit:
+                return pd.Series(x[-hist_limit:], index=col)
+            else:
+                pad = hist_limit - len(x)
+                x = x + ['0' for _ in range(pad)]
+                return pd.Series(x, index=col)
+
+        cate_df = data_df['hist_cate_list'].apply(deal).join(data_df[['cateID']]).apply(cate_encoder.transform).join(
+            data_df['label'])
+        self.data = cate_df.values
+
+    def train_valid_test_split(self, train_size=0.8, valid_size=0.1, test_size=0.1):
+        field_dims = [self.data[:-1].max().astype(int) + 1]
+        num_data = len(self.data)
+        num_train = int(train_size * num_data)
+        num_test = int(test_size * num_data)
+        train = self.data[:num_train]
+        valid = self.data[num_train: -num_test]
+        test = self.data[-num_test:]
+
+        device = self.device
+        train_x = torch.tensor(train[:, :-1], dtype=torch.long).to(device)
+        valid_x = torch.tensor(valid[:, :-1], dtype=torch.long).to(device)
+        test_x = torch.tensor(test[:, :-1], dtype=torch.long).to(device)
+        train_y = torch.tensor(train[:, -1], dtype=torch.float).unsqueeze(1).to(device)
+        valid_y = torch.tensor(valid[:, -1], dtype=torch.float).unsqueeze(1).to(device)
+        test_y = torch.tensor(test[:, -1], dtype=torch.float).unsqueeze(1).to(device)
+
+        return field_dims, (train_x, train_y), (valid_x, valid_y), (test_x, test_y)
 
 
 class BatchLoader:
